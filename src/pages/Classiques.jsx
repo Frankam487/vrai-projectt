62751ff37c12ec040822eaa5668d605b9c505da2
Frankam487 <fkamgang06@gmail.com>
2025-01-07 15:32:18 +0100

    picture added

--- src/pages/Classiques.jsx
+++ src/pages/Classiques.jsx
@@ -6,7 +6,7 @@ const Classiques = () => {
       <div className="bg-white p-6 rounded-lg shadow-xl max-w-4xl w-full">
         <div className="flex justify-center mb-6">
           <img
-            src="./public/box-men.jpg"
+            src="./box-men.jpg"
             alt="Box Braids Classiques"
             className="w-full  object-cover object-center rounded-lg shadow-lg"
           />
